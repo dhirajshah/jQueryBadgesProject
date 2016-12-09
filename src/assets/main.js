4411d4144997248a5cefb4c31728ef7704e652e9
Dhiraj Shah <Dhiraj.Shah@xceedance.com>
2016-12-09 17:50:26 +0530

    First Attempt to solve badges script

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+  	url: 'https://www.codeschool.com/users/dhirajshah.json',
+  	success: function(response){
+  		var completedCourses = response.courses.completed;
+  		for (var i=0, len=completedCourses.length; i < len; i++) {
+  			var output = '<div class="course">';
+  			output += '<h3>' + completedCourses[i].title + '</h3>';
+  			output += '<img src="'+ completedCourses[i].badge +'">';
+  			output += '<a class="btn btn-primary" href="'+ completedCourses[i].url +'" target="_blank">See Course</a>'; 
+  			output += '</div>'
+  			$('#badges').html(output);
+  		}
+  	}
+  });
 
 });
